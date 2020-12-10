17bce075f93857ed7c0b363241e472788c8b71b6
lanfeng <023@qq.com>
2020-12-10 10:19:28 +0800

    --提交工程 20.12.09--

--- git_demo/src/main/java/com/sxt.java
+++ git_demo/src/main/java/com/sxt.java
@@ -3,5 +3,6 @@ package com;
 public class sxt {
     public static void main(String[] args) {
         System.out.println("hello, github. i am lf.");
+        System.out.println("第一次修改！！！");
     }
 }
