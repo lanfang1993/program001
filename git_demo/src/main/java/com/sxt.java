e4793e91add45a79f42e1dd384c029841d8938e0
lanfeng <023@qq.com>
2020-12-10 10:28:36 +0800

    --提交工程 20.12.10--

--- git_demo/src/main/java/com/sxt.java
+++ git_demo/src/main/java/com/sxt.java
@@ -5,5 +5,6 @@ public class sxt {
         System.out.println("hello, github. i am lf.");
         System.out.println("第1次修改.");
         System.out.println("第2次修改.");
+        System.out.println("第3次修改-2020:12:10  10:25");
     }
 }
